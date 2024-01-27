b9ca317bf1018cb1f3be246f9c6a4d055c4a462a
Daniela Brailovsky <dbrailo@gmail.com>
2024-01-27 16:45:28 +0200

    Created the store

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert("submitted");
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          action=""
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter an URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            Go
+          </button>
+        </form>
+
+        {/* Browser URL history */}
+      </div>
+
+      {/* Display results */}
+    </section>
+  );
 };
 
 export default Demo;
