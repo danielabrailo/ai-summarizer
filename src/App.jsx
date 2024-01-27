40955345e339a16bf10268ce8f28ddcb0d693f41
Daniela Brailovsky <dbrailo@gmail.com>
2024-01-27 15:05:14 +0200

    Hero created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
 
 function App() {
-  return <>App</>;
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
 }
 
 export default App;
